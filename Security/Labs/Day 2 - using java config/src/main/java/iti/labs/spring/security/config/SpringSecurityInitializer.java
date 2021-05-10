0cba8ad5932d9f56ffbb3cd867d228370a82fee3
Yahya-Allam <76212827+Yahya-Allam@users.noreply.github.com>
2021-05-11 01:05:03 +0200

    Delete Security directory

--- Security/Labs/Day 2 - using java config/src/main/java/iti/labs/spring/security/config/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package iti.labs.spring.security.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //do nothing :  it will load the springSecurityFilterChain automatically.
-}
